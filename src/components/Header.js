ffdf8ce7243274ea162a359aef6ad15feb00c94a
Spoty-O <korotysh16@ukr.net>
2024-08-25 13:56:03 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FaBasketShopping } from "react-icons/fa6";
 import Order from "./Order";
 
-
 export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false);
 
@@ -16,23 +15,20 @@ export default function Header(props) {
               onClick={() => setCartOpen((cartOpen = !cartOpen))}
               className={`shop-cart-button ${cartOpen && "active"}`}
             />
-
-            {cartOpen && (
-            <div className="shop-cart">
-              {props.orders.map(el => (
-                <Order key = {el.id} item = {el} />
-              ))}
-            </div>
-              )}
-
           </li>
           <li>Про нас</li>
           <li>Наші Контакти</li>
           <li>Кабінет</li>
         </ul>
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.map((el) => (
+              <Order key={el.id} item={el} />
+            ))}
+          </div>
+        )}
       </div>
       <div className="presentation"></div>
     </header>
   );
 }
-
